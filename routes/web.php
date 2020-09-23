2605a0d2df5b76ddbed31c62c88e4ef57a9a2eb8
Billie <quiinthieu@gmail.com>
2020-09-23 20:28:19 +0700

    Version 1.1.1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/about', function () {
+    return view('drcare.about');
+})->name('about');
+Route::get('/appointment', function() {
+    return view('drcare.appointment');
+})->name('appointment');
+Route::get('/blog', function() {
+    return view('drcare.blog');
+})->name('blog');
+Route::get('/blog-single', function() {
+    return view('drcare.blog-single');
+})->name('blog-single');
+Route::get('/contact', function() {
+    return view('drcare.contact');
+})->name('contact');
+Route::get('/department', function() {
+    return view('drcare.department');
+})->name('department');
+Route::get('/doctor', function() {
+    return view('drcare.doctor');
+})->name('doctor');
+Route::get('/index', function() {
+    return view('drcare.index');
+})->name('index');
+Route::get('pricing', function() {
+    return view('drcare.pricing');
+})->name('pricing');
