6f7eda2ef9f479a1f11613521afbb96eb18fd82f
Billie <quiinthieu@gmail.com>
2020-09-25 17:56:38 +0700

    Version 1.1.6

--- resources/views/drcare/appointment.blade.php
+++ resources/views/drcare/appointment.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.drcare')
 @section('content')
-    <x-jumbotron title="Appointment" />
+    <x-jumbotron title="Appointment" sub-title="Appointment" />
     <x-free-consultation-form />
 @endsection
