061650729d8dd65bc50be894d6a2fa28e95d1461
Billie <quiinthieu@gmail.com>
2020-10-03 12:52:17 +0700

    Version 1.3.3

--- resources/views/drcare/services.blade.php
+++ resources/views/drcare/services.blade.php
@@ -15,48 +15,59 @@
                         have been rewritten a thousand times and everything that was left from its origin would be the
                         word.</p>
                     <div class="row mt-5 pt-2">
-                        @foreach()
+                        @foreach(\App\Models\ServiceType::all() as $serviceType)
+                            <div class="col-lg-6">
+                                <div class="services-2 d-flex">
+                                    <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+                                            class="flaticon-first-aid-kit"></span></div>
+                                    <div class="text">
+                                        <h3>{{$serviceType->name}}</h3>
+                                        <p>{{$serviceType->description}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
 
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-                                        class="flaticon-first-aid-kit"></span></div>
-                                <div class="text">
-                                    <h3>Primary Care</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-                                        class="flaticon-dropper"></span></div>
-                                <div class="text">
-                                    <h3>Lab Test</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-                                        class="flaticon-experiment-results"></span></div>
-                                <div class="text">
-                                    <h3>Symptom Check</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="services-2 d-flex">
-                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-                                        class="flaticon-heart-rate"></span></div>
-                                <div class="text">
-                                    <h3>Heart Rate</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-                                </div>
-                            </div>
-                        </div>
+{{--                        <div class="col-lg-6">--}}
+{{--                            <div class="services-2 d-flex">--}}
+{{--                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span--}}
+{{--                                        class="flaticon-first-aid-kit"></span></div>--}}
+{{--                                <div class="text">--}}
+{{--                                    <h3>Primary Care</h3>--}}
+{{--                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-lg-6">--}}
+{{--                            <div class="services-2 d-flex">--}}
+{{--                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span--}}
+{{--                                        class="flaticon-dropper"></span></div>--}}
+{{--                                <div class="text">--}}
+{{--                                    <h3>Lab Test</h3>--}}
+{{--                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-lg-6">--}}
+{{--                            <div class="services-2 d-flex">--}}
+{{--                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span--}}
+{{--                                        class="flaticon-experiment-results"></span></div>--}}
+{{--                                <div class="text">--}}
+{{--                                    <h3>Symptom Check</h3>--}}
+{{--                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-lg-6">--}}
+{{--                            <div class="services-2 d-flex">--}}
+{{--                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span--}}
+{{--                                        class="flaticon-heart-rate"></span></div>--}}
+{{--                                <div class="text">--}}
+{{--                                    <h3>Heart Rate</h3>--}}
+{{--                                    <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
                     </div>
                 </div>
             </div>
