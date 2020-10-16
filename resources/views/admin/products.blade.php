431e866a322df2f7a80b8a9082d50623bfd957ac
Billie <quiinthieu@gmail.com>
2020-10-16 22:48:02 +0700

    Version 1.8.0

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -12,13 +12,42 @@
                         @foreach($products as $product)
                             <div class="col-3">
                                 <div class="card mb-3">
-                                    <img class="img-fluid card-img-top"
-                                         src="{{asset(json_decode($product->photos)[0])}}"
-                                         alt="Card image cap">
+                                    <div id="{{'carousel' . $product->id}}" class="carousel slide" data-ride="carousel"
+                                         data-interval="false">
+                                        <ol class="carousel-indicators">
+                                            @foreach(json_decode($product->photos) as $key => $val)
+                                                <li data-target="{{'#carousel' . $product->id}}"
+                                                    data-slide-to="{{$key}}"
+                                                    class="{{$key === 0 ? "active" : ""}} text-dark"
+                                                    style="background-color: rgba(0, 0, 0, 0.5);"></li>
+                                            @endforeach
+                                        </ol>
+                                        <div class="carousel-inner">
+                                            @foreach(json_decode($product->photos) as $key => $val)
+                                                <div class="carousel-item {{$key === 0 ? "active" : ""}}">
+                                                    <img class="d-block w-100"
+                                                         src="{{asset($val)}}">
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                        <a class="carousel-control-prev" href="{{'#carousel' . $product->id}}"
+                                           role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"
+                                                  style="filter: invert();"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="{{'#carousel' . $product->id}}"
+                                           role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"
+                                                  style="filter: invert();"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
                                     <div class="card-body">
                                         <h5 class="card-title">{{$product->name}}</h5>
                                         <p class="card-text">{{$product->description}}</p>
-                                        <span class="badge badge-secondary">{{\Illuminate\Support\Facades\DB::table('categories')->find($product->category_id)->name}}</span>
+                                        <span
+                                            class="badge badge-secondary">{{\Illuminate\Support\Facades\DB::table('categories')->find($product->category_id)->name}}</span>
                                     </div>
                                     <div class="card-footer">
                                         <a class="btn btn-outline-primary btn-sm" role="button" href="#">Edit</a>
